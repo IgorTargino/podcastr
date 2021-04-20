55065c60124e52400993af4b4b8510c7c8e54bed
IgorTargino <igortargino01@gmail.com>
2021-04-20 14:49:55 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { useEffect } from 'react'
+import Header from './components/Header'
+
+export default function Home(props) {
+
+  return (
+    <h1>Index</h1>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, 
+  }
+}
